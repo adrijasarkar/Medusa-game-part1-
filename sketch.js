6ea1cd7feb995b305286165adfdceb8aba23d299
adrijasarkar <76274422+adrijasarkar@users.noreply.github.com>
2021-06-13 11:06:10 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var Medusa
+var dragon
+var demon
+
+
+function setup() {
+  createCanvas(displayWidth,displayHeight);
+  createSprite(400, 200, 50, 50);
+
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
+
+function preload (){
+
+}
